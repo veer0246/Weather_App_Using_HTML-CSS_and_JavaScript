7c775e4696551a780e9c0689be90cbe1b06781ef
veer0246 <veer009292@gmail.com>
2024-08-07 15:16:28 +0530

    fetch api data

---
+++ script.js
@@ -0,0 +1,22 @@
+// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=lucknow&appid=f424b9c9d2c753461cd7043f41e3a8be&units=metric"
+
+
+//fetch api to fide the data------
+const apiKey = "f424b9c9d2c753461cd7043f41e3a8be";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+async function checkWeather(city){  
+    const result = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await result.json();
+
+    console.log(data)
+
+    //data set on element-------
+    document.querySelector('.city').innerHTML = data.name
+    // document.querySelector('.temp').innerHTML = data.main.temp + "°c"
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp )+ "°c"
+    document.querySelector('.humidity').innerHTML = data.main.humidity + "%"
+    document.querySelector('.wind').innerHTML = data.wind.speed + " Km/h"
+}
+
+checkWeather();
